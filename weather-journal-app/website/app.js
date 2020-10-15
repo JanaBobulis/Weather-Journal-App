a04febc9d6e59a3ff3cf6892aeca5725eee94348
JanaBobulis <71527795+JanaBobulis@users.noreply.github.com>
2020-10-15 16:30:51 +0100

    Update app.js

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -6,15 +6,15 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
 let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-let apiKey = '&units=metric&appid=e4556829179c13d93b039efb3a3c764f';
+let apiKey = '&units=metric&APPID=e4556829179c13d93b039efb3a3c764f';
 
 //Write an async function in app.js that uses fetch() to make a GET request to the OpenWeatherMap API.
-const getData = async (baseURL, zip, key)=>{
-    const res = await fetch(baseURL + zip + key)
+const getData = async (baseURL, zip, apiKey)=>{
+    const res = await fetch(baseURL + zip + apiKey);
     try {
-        const projectdata = await res.json();
-        console.log(projectdata);
-        return projectdata;
+        const data = await res.json();
+        console.log(data);
+        return data;
     } catch(error) {
         console.log("error", error);
     }
@@ -24,32 +24,34 @@ const getData = async (baseURL, zip, key)=>{
 document.getElementById('generate').addEventListener('click', performAction);
 function performAction(e) {
     let zip = document.getElementById('zip').value;
-    let response = document.getElementById('feelings').value;
+    let feelings = document.getElementById('feelings').value;
     getData(baseURL, zip, apiKey)
 
     .then(function(data){
         console.log(data)
-        postData('/addData', {temp: data.temp, response: data.response, date:newDate} );
+        postData('/addData', {temperature: data.temp, response: feelings, date: newDate})
+    .then(function() {
+        updateUI()
     })
-    .then(updateUI())
-    }
+})
+}
 
 // After your successful retrieval of the weather data, you will need to chain another Promise that makes a POST request to add the API data, as well as data entered by the user, to your app. You will need to write another async function to make this POST request. The function should receive a path and a data object. The data object should include temperature, date, user response
 const updateUI = async() => {
-    const request = await fetch('/all');
+    const req = await fetch('http://localhost:3030/all');
     try{
-        const allData = await request.json();
+        const allData = await req.json();
         document.getElementById('date').innerHTML = allData.date;
-        document.getElementById('zip').innerHTML = allData.temperature;
-        document.getElementById('feelings').innerHTML = allData.response;
+        document.getElementById('temp').innerHTML = allData.temperature;
+        document.getElementById('content').innerHTML = allData.response;
     }catch(error){
         console.log("error", error);
     }
 }
 
 //Finally, chain another Promise that updates the UI dynamically Write another async function that is called after the completed POST request. This function should retrieve data from our app, select the necessary elements on the DOM (index.html), and then update their necessary values to reflect the dynamic values for: Temperature, Date, User input
-const postData = async ( url ='', data = {}) => {
-    const response = await fetch(url, {
+const postData = async (url ='', data = {}) => {
+    const res = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
@@ -58,12 +60,12 @@ const postData = async ( url ='', data = {}) => {
         body: JSON.stringify(data),
     });
     try {
-        const newData = await response.json();
+        const newData = await res.json();
         console.log(newData);
             return newData;
     } catch(error) {
         console.log('error', error);
     }
 }
-//postData('/add', {answer:42});
+
 
